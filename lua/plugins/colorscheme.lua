a24b394fdaf756821931005628d0838762237ebb
Leandro <leandrofuerte75@gmail.com>
2025-08-02 23:46:10 -0400

    catppuccin and obsidian

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,4 +9,20 @@ return {
       }
     end,
   },
+  {
+    "Gentleman-Programming/gentleman-kanagawa-blur",
+    name = "gentleman-kanagawa-blur",
+    priority = 1000,
+    variant = "sakura",
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      transparent_background = true, -- disables setting the background color.
+      term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
+    },
+  },
 }
